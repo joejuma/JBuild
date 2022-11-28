8ede774a84cf52dfcd30d7aeb16d861e142e0931
Joseph Juma <joejstudio@gmail.com>
2022-11-27 23:36:53 -0800

    Version 0.1.4: Added basic process argument handling.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+const path = require("path");
 const NodeGraph = require("./NodeGraph/NodeGraph_class.js");
 
+/*
 let graph = new NodeGraph();
 
 let root = graph.addNode("root");
@@ -16,3 +19,22 @@ root.addParent(dep3);
 console.log(graph);
 console.log("Dependency sequence:");
 graph.getNodeDependencySequence().map( n => console.log(n.name));
+*/
+
+const main = () => {
+    console.log(process.argv);
+    if( process.argv.length >= 4 ){
+        let entryFile = path.resolve(process.argv[2]);    // 1st Argument
+        let outputFile = path.resolve(process.argv[3]);   // 2nd Argument
+
+        // Start your recursive traversal in the entry-file.
+        // Pretty much djkstra from here...
+        // @todo: add behavior for recursively stepping through files and building the graph
+        // similar to the above commented out graph.
+    }
+    else {
+        console.error("Insufficient number of arguments passed.");
+    }
+};
+
+main();
